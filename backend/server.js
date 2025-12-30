453546fd11519a9485fad0afbf7f52b8fe9b7328
Sonali Sahu <sonalisahu0064@gmail.com>
2025-12-30 20:43:37 +0530

    Added deployed frontend link into the cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 /* -------------------- MIDDLEWARES -------------------- */
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://merit-auth.vercel.app" ],
   methods: ["GET", "POST", "DELETE", "PATCH"],
   credentials: true,
 };
